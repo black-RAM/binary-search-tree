7c0c71f7af65a616d3db36e8a252b828e14e5003
black-RAM <pplanes311@gmail.com>
2024-02-06 10:29:54 +0300

    Tree class, buildTree, parseTree

--- index.js
+++ index.js
@@ -5,3 +5,34 @@ class Node{
     this.right = null
   }
 }
+
+class Tree{
+  constructor(array) {
+    this.root = this.buildTree(array.sort((a, b) => a - b), 0, array.length - 1) 
+  }
+
+  buildTree(arr, start, end) {
+    if(start > end) return null
+
+    const mid = Math.floor((start + end) / 2)
+    const root = new Node(arr[mid])
+    root.left = this.buildTree(arr, start, mid - 1)
+    root.right = this.buildTree(arr, mid + 1, end)
+    return root
+  }
+
+  parseTree(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) return
+    if (node.right !== null) {
+      this.parseTree(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false)
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`)
+    if (node.left !== null) {
+      this.parseTree(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true)
+    }
+  }
+}
+
+const randomNumbers = Array.from({length: 12}, () => parseInt(Math.random() * 100))
+const bst = new Tree(randomNumbers)
+bst.parseTree()
