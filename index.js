65c27c9afe7a43987dee2d96ff2da960cb920c3f
black-RAM <pplanes311@gmail.com>
2024-02-09 21:01:51 +0300

    final tests for assignment

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ class Node{
 
 class Tree{
   constructor(array) {
-    this.root = this.buildTree(array.sort((a, b) => a - b), 0, array.length - 1) 
+    const input = [...new Set(array.sort((a, b) => a - b))]
+    this.root = this.buildTree(input, 0, array.length - 1) 
   }
 
   buildTree(arr, start, end, parent = null) {
@@ -27,7 +28,7 @@ class Tree{
   }
 
   insert(value, node = this.root, parent = null) {
-    if(node == null) return new Node(value)
+    if(node == null) return new Node(value, parent)
     const sub = node.assignSubtree(value)
     node[sub] = this.insert(value, node[sub], node)
     return node
@@ -36,49 +37,6 @@ class Tree{
   find(value, node = this.root) {
     if(node == null || node.data == value) return node
     return this.find(value, node[node.assignSubtree(value)])
-  }
-
-  delete(value, node = this.root) {
-    node = this.find(value, node)
-    if(node == null) return
-
-    // node has one child or no children
-    if(!(!!node.left && !!node.right)) {
-      const heir = node[!node.left ? "right" : "left"]
-
-      if(node.parent) {
-        if(node.parent.left == node) {
-          node.parent.left = heir
-        } else {
-          node.parent.right = heir
-        }
-      } else {
-        this.root = heir
-      }
-
-      if(heir) {
-        heir.parent = node.parent
-      }
-    } else{
-      // node has two children
-      let successor = node.right
-
-      // leftmost node of right subtree is successor
-      while(successor.left !== null) {
-        successor = successor.left
-      }
-
-      // shift right child of successor
-      if(successor.parent == node) {
-        successor.parent.right = successor.right
-      } else {
-        successor.parent.left = successor.right
-      }
-
-      // copy successor data to "deleted" node
-      node.data = successor.data
-      successor = null
-    }
   }
 
   levelOrder(mutator = (el => el), node = this.root) {
@@ -175,15 +133,73 @@ class Tree{
     this.insert(value)
     this.rebalance()
   }
+
+  delete(value) {
+    node = this.find(value)
+    if(node == null) return
+
+    // node has one child or no children
+    if(!(!!node.left && !!node.right)) {
+      const heir = node[!node.left ? "right" : "left"]
+
+      if(node.parent) {
+        if(node.parent.left == node) {
+          node.parent.left = heir
+        } else {
+          node.parent.right = heir
+        }
+      } else {
+        this.root = heir
+      }
+
+      if(heir) {
+        heir.parent = node.parent
+      }
+    } else{
+      // node has two children
+      let successor = node.right
+
+      // leftmost node of right subtree is successor
+      while(successor.left !== null) {
+        successor = successor.left
+      }
+
+      // shift right child of successor
+      if(successor.parent == node) {
+        successor.parent.right = successor.right
+      } else {
+        successor.parent.left = successor.right
+      }
+
+      // copy successor data to "deleted" node
+      node.data = successor.data
+      successor = null
+    }
+  }
 }
 
-const inputs = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 67, 6345, 324]
-const bst = new Tree([1])
-for (const input of inputs) {
-  bst.insert(input)
+// driver script
+let input = Array.from({length: 20}, () => parseInt(Math.random() * 100))
+const bst = new Tree(input)
+console.log("Binary Search Tree: ")
+bst.parseTree()
+console.log("Tree is balanced: ", bst.isBalanced())
+console.log("Level order: ", bst.levelOrder())
+console.log("Pre-order: ", bst.preOrder())
+console.log("Post-order: ", bst.postOrder())
+console.log("In-order: ", bst.inOrder())
+
+input = Array.from({length: 20}, () => 100 + parseInt(Math.random() * 100))
+for (const n of input) {
+  bst.insert(n)
 }
 bst.parseTree()
-console.log("Tree balanced? ", bst.isBalanced())
+console.log("Tree is balanced: ", bst.isBalanced())
+console.log("Re-balancing...")
 bst.rebalance()
 bst.parseTree()
-console.log("Tree balanced? ", bst.isBalanced())
+console.log("Tree is balanced: ", bst.isBalanced())
+console.log("Level order: ", bst.levelOrder())
+console.log("Pre-order: ", bst.preOrder())
+console.log("Post-order: ", bst.postOrder())
+console.log("In-order: ", bst.inOrder())
